5c9fa4cd5d5bf7f95b121e66183d7b5b608a797f
Rishi <hello@rishispeets.com>
2019-04-27 11:21:52 +0200

    refactor parser

--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,18 @@
 const htmlparser = require("htmlparser2");
-const { isTargetLink } = require("./utils");
+const { isErrorFareLink } = require("./utils");
 
 module.exports = function parsePage(html) {
   if (!html || typeof html !== "string") throw new Error("Provide valid html.");
-  const links = new Set();
 
-  const parser = createParser(addLinkTo(links));
-  parse(parser, html);
-
-  return Array.from(links);
+  const errorFareLinks = new Set();
+  findErrorFareLinks(errorFareLinks, html);
+  return Array.from(errorFareLinks);
 };
+
+function findErrorFareLinks(links, html) {
+  const parser = createParser(addLinkTo(links));
+  parseHtml(parser, html);
+}
 
 function createParser(onOpenTagHandler) {
   return new htmlparser.Parser(
@@ -18,13 +21,13 @@ function createParser(onOpenTagHandler) {
   );
 }
 
-function parse(parser, html) {
+function parseHtml(parser, html) {
   parser.write(html);
   parser.end();
 }
 
 function addLinkTo(links) {
   return (name, attribs) => {
-    if (isTargetLink(name, attribs)) links.add(attribs.href);
+    if (isErrorFareLink(name, attribs)) links.add(attribs.href);
   };
 }
